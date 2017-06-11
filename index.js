adc41a0d8a00b00303a5b7b38f3a6e705bae9eaa
Thomas Shen <Thomas Shen>
2017-06-11 14:50:54 -0400

    node backend

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+// db setup
+
+mongoose.connect('mongodb://localhost:auth/auth');
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+router(app);
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server is listening on: ", port);
